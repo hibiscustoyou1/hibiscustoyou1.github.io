f98d4f0ad51292e7ecdb4697802ac1821bfee472
zhangxietao <zhangxt7@chinatelecom.cn>
2025-02-25 16:02:21 +0800

    feat: my-element@250225.2

---
+++ my-element/genUrls.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+
+const recursion = (route, name) => {
+  const src = route + name;
+  fs.stat(src, (err, stats) => {
+    if (err) {
+      return
+    }
+    if (stats.isDirectory()) {
+      fs.readdir(src, (err, files) => {
+        if (err) {
+          return
+        }
+        files.forEach(item => {
+          recursion(src + '/', item);
+        })
+      })
+    } else {
+      console.log({
+        url: src.replace('./demo', 'https://hibiscustoyou.gitee.io/code-hosting/demo'),
+        name: path.basename(src, '.html')
+      }, ',');
+    }
+  })
+}
+
+recursion('./demo', '');
